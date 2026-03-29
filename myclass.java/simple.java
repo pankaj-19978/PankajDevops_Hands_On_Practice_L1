37300e8ea2bb17fbbe13e7d27c73456f0337422d
pankaj bhatra <bhatrapankaj568@gmail.com>
2026-03-28 22:29:07 -0400

    this is a java file

---
+++ myclass.java/simple.java
@@ -0,0 +1,6 @@
+public class MyFirstProgram {
+    public static void main(String[] args) {
+        // स्क्रीन पर मैसेज प्रिंट करने के लिए
+        System.out.println("Hello, Welcome to Java Practice!");
+    }
+}
