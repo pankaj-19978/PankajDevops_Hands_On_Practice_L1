6f7342b496c7c4daa87e37b837ffb9cde516ec90
PANKAJ BHATRA <bhatrapankaj568@gmail.com>
2026-06-20 06:19:06 -0700

    Delete myclass.java directory

--- myclass.java/simple.java
+++
@@ -1,6 +0,0 @@
-public class MyFirstProgram {
-    public static void main(String[] args) {
-        // This change was made in branch 1.
-        System.out.println("Hello, Welcome to Java Practice! this is branch 1 update.");
-    }
-}
